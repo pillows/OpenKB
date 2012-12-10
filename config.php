d10b0378635a46ad7de187fc52aad14446a5e593
Pillows <matthew.wong@outlook.com>
2012-12-10 09:28:47 -0500

    Added PDO Connection

--- config.php
+++ config.php
@@ -1,16 +1,10 @@
 <?php
-$sqluser = "root";
-$sqlpass = "xoxolacy";
-$sqldb = "openkb";
-$sqlserver = "localhost";
-
-$openkb_connect = mysql_connect($sqlserver, $sqluser, $sqlpass);
-
-
-if(!$openkb_connect)
+$username = "";
+$password = "";
+$conn = new PDO('mysql:host=localhost;dbname=openkb', $username, $password);
+if(!$conn)
 {
 	die("<div style = 'color:red;font-size:100px;'>Sorry but database details are incorrect</div>");
 }
-mysql_select_db($sqldb) or die(mysql_error());
 
 ?>
